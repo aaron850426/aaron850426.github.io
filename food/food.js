cb43613e380dc280ab495a9242df0029ef484f3a
aaron850426 <aaron850426@gmail.com>
2016-06-16 23:34:56 +0800

    WPFP_201606162334

--- food/food.js
+++ food/food.js
@@ -15,7 +15,11 @@
 var food = [["八方雲集", "QQ魯肉飯", "雅盧"],
             ["河豚很多", "竹間", "義家之煮"],
             ["美而美早餐", "美芝城早餐"],
-            ["一粒咖啡"]];
+            ["一粒咖啡", "comebuy", "清心"]];
+var address = [["202基隆市中正區新豐街416號", "202基隆市中正區新豐街405號", "202基隆市中正區北寧路154巷23號"],
+            ["202基隆市中正區正濱路88號", "201基隆市信義區深溪路80號", "201基隆市信義區深溪路73號"],
+            ["202基隆市中正區祥豐街843號", "202基隆市中正區祥豐街849號" ],
+            ["202基隆市中正區北寧路242-6號", "20248基隆市中正區中正路820號", "基隆市中正區中正路782號"]];
 var random_value;
 var arrayIndex;
 
@@ -24,7 +28,6 @@ function start(){
   initMap();
   var form3 = document.getElementById("three").checked;
   document.getElementById( "choose_button" ).addEventListener( "click", rand_pic, false );
-  document.getElementById( "menu_button" ).addEventListener( "click", rand_pic_menu, false );
 }
 window.addEventListener( "load", start, false );
 
@@ -41,18 +44,25 @@ function rand_pic(){
   }
   random_value = Math.floor( Math.random() * food[arrayIndex].length );
   document.getElementById( "choose_pics" ).innerHTML = "<img src = 'food_img/" + food[arrayIndex][random_value] + "_招牌.jpg'/>";
+  document.getElementById("address").value = address[arrayIndex][random_value];
+  geocodeAddress(geocoder, map);
 }
 
-function rand_pic_menu(){
-  document.getElementById( "menu_pics" ).innerHTML = "<img src = 'food_img/" + food[arrayIndex][random_value] + "_菜單.jpg'/>";
+
+// Modal Image Gallery
+function onClick() {
+  document.getElementById("img01").src = "food_img/" + food[arrayIndex][random_value] + "_菜單.jpg";
+  document.getElementById("modal01").style.display = "block";
 }
 
+var map;
+var geocoder;
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
     center: {lat: -34.397, lng: 150.644}
   });
-  var geocoder = new google.maps.Geocoder();
+  geocoder = new google.maps.Geocoder();
   geocodeAddress(geocoder, map);
   document.getElementById('submit').addEventListener('click', function() {
     geocodeAddress(geocoder, map);
